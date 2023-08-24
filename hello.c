a1e3bce3a420b031e3e04883a2ef8042abb00b99
keilani.mielke <kmielke3@huskers.unl.edu>
2023-08-24 10:43:12 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv) {
 
     printf("William Bryant\n");
     printf("Computer Enginerring!\n");
+    printf("Keilani Mielke\n");
+    printf("Computer Enginerring and Computer Science!\n");
 
     return 0;
 }
